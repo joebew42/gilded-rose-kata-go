8241f460389a51d0957f1a6cf01a82968049d6cc
dc0d <kaveh.shahbazian@gmail.com>
2020-05-17 11:04:00 +0200

    first test for aged brie

--- gildedrose_test.go
+++ gildedrose_test.go
@@ -1,13 +1,31 @@
 package gildedrose
 
-import "testing"
+import (
+	"testing"
 
-func TestXXX(t *testing.T) {
+	assert "github.com/stretchr/testify/require"
+)
+
+func Test_cover_aged_brie(t *testing.T) {
+	var (
+		assert = assert.New(t)
+	)
+
 	var (
 		item Item
+
+		expectedQuality = 2
+		expectedSellIn  = -1
 	)
 
 	{
-
+		item = Item{
+			name: "Aged Brie",
+		}
 	}
+
+	UpdateQuality([]*Item{&item})
+
+	assert.Equal(expectedQuality, item.quality)
+	assert.Equal(expectedSellIn, item.sellIn)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/joebew42/kata1
 
 go 1.14
+
+require github.com/stretchr/testify v1.5.1
